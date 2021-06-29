f9f9e6ecbb638937315260d48a8ae83b2ef2f93e
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2021-06-29 08:45:13 +0200

    not found route #50 (#55)
    * not found route #50
    
    * write tests
    
    * trigger CI

--- .travis/tests/RouteChange_constructor.js
+++ .travis/tests/RouteChange_constructor.js
@@ -1,5 +1,5 @@
 /* global global RouteChange */
 (() => {
     global.routeChange = Object.create(RouteChange)
-        .constructor(global.params.type, global.params.path, global.params.state);
+        .constructor(global.params.type, global.params.path, global.params.state, global.params.isLeaf);
 })();
